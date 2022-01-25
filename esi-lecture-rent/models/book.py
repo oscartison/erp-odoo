00610534e9cbc0de7a1ef58e94786fc70a47acd0
Oscar Tison <55315@etu.he2b.be>
2022-01-25 09:53:51 +0100

    question 3 + required question 2

--- esi-lecture-rent/models/book.py
+++ esi-lecture-rent/models/book.py
@@ -1,6 +1,12 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Book(models.Model):
     _inherit = 'esi.book'
     rent_ids = fields.Many2one('esi.book.rent', string='emprunts')
+    nb_rents = fields.Integer("Nombre d'emprunts", compute="_count_rents")
+
+    @api.depends('likes')
+    def _count_rents(self):
+        for book in self:
+            book.nb_rents = len(book.rent_ids)
--- esi-lecture-rent/models/book_rent.py
+++ esi-lecture-rent/models/book_rent.py
@@ -3,8 +3,8 @@ from odoo import fields, models, exceptions, api
 class Rent(models.Model):
     _name = 'esi.book.rent'
 
-    book_id = fields.One2many('esi.book','rent_ids', string="Livre")
-    member_id = fields.One2many('res.partner','rent_ids', string="Emprunteur", domain=[('member', '=', True)])
+    book_id = fields.One2many('esi.book','rent_ids', string="Livre", required = True)
+    member_id = fields.One2many('res.partner','rent_ids', string="Emprunteur", domain=[('member', '=', True)], required = True)
     state = fields.Selection(
         [('prete', 'Exemplaire prêté'), ('retourn', 'Exemplaire retourné'), ('perdu', 'Exemplaire perdu')], default='prete')
     date_pret = fields.Datetime(
