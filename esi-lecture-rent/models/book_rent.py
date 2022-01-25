e8f31a276535d1e810f9600e357d2ebe7170464d
Oscar Tison <55315@etu.he2b.be>
2022-01-25 11:47:03 +0100

    corrections vues

--- esi-lecture-rent/models/book_rent.py
+++ esi-lecture-rent/models/book_rent.py
@@ -15,12 +15,12 @@ class Rent(models.Model):
     return_date = fields.Date(
         "Date de retour")
 
-    # @api.constrains("book_id")
-    # def check_rent(self):
-    #     for book in self.book_id:
-    #         for rent in book.rent_ids:
-    #             if rent.state != 'retour':
-    #                 raise exceptions.ValidationError("Ce livre n'a pas encore été rendu !")
+    @api.constrains("book_id")
+    def check_rent(self):
+    
+        for rent in self.book_id.rent_ids:
+            if rent and not rent.state == 'retour':
+                raise exceptions.ValidationError("Ce livre n'a pas encore été rendu !")
 
     def setRetour(self):
         self.state = 'retour'
